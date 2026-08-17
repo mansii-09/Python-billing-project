a34b413194cf2785a6950c1a7ec1e0c0900b0515
mansi goswami <goswamimansi2006@gmail.com>
2026-08-17 18:37:01 +0530

    Add WhiteNoise for static files

--- Biling/settings.py
+++ Biling/settings.py
@@ -52,6 +52,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -59,7 +60,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
 ROOT_URLCONF = 'Biling.urls'
 
 TEMPLATES = [
